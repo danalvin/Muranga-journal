b4f4e91fe18f0739670cef8ed1aef610d8bc88be
danalvin <mungaiwaituika@gmail.com>
2020-01-24 12:12:20 +0300

    new changes

--- blog/urls.py
+++ blog/urls.py
@@ -1,1 +1,9 @@
-from django
+from django.urls import path
+from .views import *
+
+app_name = "blog"
+
+urlpatterns = [
+    path('blog', BlogList.as_view(), name="blog"),
+    path('blog/<int:id>', BlogDetailView.as_view(), name="blog-details"),
+]
